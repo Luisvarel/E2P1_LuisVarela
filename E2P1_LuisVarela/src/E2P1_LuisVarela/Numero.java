2da80cee25098c1d39b9f19339f72a601acfdd1a
Luisvarel <132074260+Luisvarel@users.noreply.github.com>
2023-06-23 17:51:45 -0600

    Add files via upload

---
+++ E2P1_LuisVarela/src/E2P1_LuisVarela/Numero.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package E2P1_LuisVarela;
+
+/**
+ *
+ * @author Luis Andres Varela
+ */
+public class Numero {
+    private int base;
+    private int numero;
+    public Numero(int base,int numero) {
+        
+    }
+    
+    
+}
